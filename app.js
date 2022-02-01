cf863fe175ba128fa24d2684379be946ac4090cc
MF Safi <mfsafi@MacBook-Pro.local>
2022-02-01 03:24:21 +0100

    First Commit - Newsletter_Signup_Project

---
+++ app.js
@@ -0,0 +1,39 @@
+
+const express = require("express");
+const https = require("https");
+const request = require("request");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/signup.html")
+});
+
+app.post("/", function(req, res){
+
+  let firstName = req.body.fName;
+  let lastName = req.body.lName;
+  let email = req.body.email;
+
+console.log(firstName, lastName, email);
+
+})
+
+//response.send();
+
+
+
+
+
+app.listen(3006, function(res) {
+  console.log("Newsletter-Signup Server is running on port 3005")
+})
+
+
+// API Key for mailChimps>
+// 8f3279cde5ba44d56ddf3b90723468b6-us14
